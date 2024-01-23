08fde4e9ffca19f20e489845f98df23abaf7b574
Sebastian Micko <Micko.S@etoncollege.org.uk>
2024-01-23 22:18:07 +0000

    did everything except for last 2 challenges

---
+++ CarCustomiser/Car.swift
@@ -0,0 +1,8 @@
+//
+//  Car.swift
+//  CarCustomiser
+//
+//  Created by Micko, Sebastian (AMM) on 23/01/2024.
+//
+
+import Foundation
---
+++ CarCustomiser/StarterCars.swift
@@ -0,0 +1,8 @@
+//
+//  StarterCars.swift
+//  CarCustomiser
+//
+//  Created by Micko, Sebastian (AMM) on 23/01/2024.
+//
+
+import Foundation
