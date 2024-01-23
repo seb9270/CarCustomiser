c07d19f6fcceae7e4fca8cd6ff1e2122fc4594ba
Sebastian Micko <Micko.S@etoncollege.org.uk>
2024-01-23 17:06:26 +0000

    Initial Commit

---
+++ CarCustomiser/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  CarCustomiser
+//
+//  Created by Micko, Sebastian (AMM) on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
